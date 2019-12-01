dfa0c678d85989319371868539a232082ddd3f67
Kathia Mgwaba <mgwaba68@gmail.com>
2019-12-01 13:38:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  lowest = []
+  
+  name_hash do |key|
+    if key < 0
+      key
+    else
+      return name_hash
+  end
 
 end
