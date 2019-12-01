049982e213009a65e36fe454ddc36a0ec7b9e67c
Kathia Mgwaba <mgwaba68@gmail.com>
2019-12-01 13:49:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   low_key = nil
 
   name_hash.each do |key, value|
-    if lowest == 0 || value < low_key
+    if lowest == 0 || value < lowest
       lowest = value
       low_key = key
     end
